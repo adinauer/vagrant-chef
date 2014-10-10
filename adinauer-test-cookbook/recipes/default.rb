753ce5050c4097231bb2cbde333c06cbea9600a2
Alexander Dinauer <alexander@dinauer.at>
2014-10-10 07:25:22 +0200

    use /home/vagrant/hello.txt for the templates target file

--- adinauer-test-cookbook/recipes/default.rb
+++ adinauer-test-cookbook/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-template "/vagrant/hello.txt" do
+template "/home/vagrant/hello.txt" do
 	source "mytemplate.erb"
 	variables({
 		:who => node[:who]
--- adinauer-test-cookbook/spec/unit/recipes/default_spec.rb
+++ adinauer-test-cookbook/spec/unit/recipes/default_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe 'adinauer-test-cookbook::default' do
-	let(:chef_run) do ChefSpec::Runner.new.converge(described_recipe) end
+	let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+	let(:hello_file) { '/home/vagrant/hello.txt' }
 
-	it 'creates /vagrant/hello.txt' do
-		expect(chef_run).to render_file('/vagrant/hello.txt')
+	it 'creates hello.txt' do
+		expect(chef_run).to render_file(hello_file)
 	end
 
-	it 'creates /vagrant/hello.txt using Someone as default parameter' do
-		expect(chef_run).to render_file('/vagrant/hello.txt')
+	it 'creates hello.txt using Someone as default parameter' do
+		expect(chef_run).to render_file(hello_file)
 			.with_content('Someone was here')
 	end
 
-	it 'creates /vagrant/hello.txt with joe if attribute is specified' do
+	it 'creates hello.txt with joe if attribute is specified' do
 		chef_run.node.set['who'] = 'Joe'
 		chef_run.converge(described_recipe)
 
-		expect(chef_run).to render_file('/vagrant/hello.txt')
+		expect(chef_run).to render_file(hello_file)
 			.with_content('Joe was here')
 	end
 end
